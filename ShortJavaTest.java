04aadfb1024efc269590e93d5f11acd72d0393d6
K-A <kapeart@gmail.com>
2017-12-30 14:56:23 -0500

    Initial local commit

---
+++ ShortJavaTest.java
@@ -0,0 +1,13 @@
+/*
+ * ShortJavaTest.java
+ * 
+ * A short program to test if Java is working on a machine.
+ * 
+ * 
+ * 
+ */
+public class ShortJavaTest {
+    public static void main(String[] args) {
+        System.out.println("Hello there! This is a ShortJavaTest.");
+    }
+}
